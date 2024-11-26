878e868e0c12d9346abb947648d25aef6924afc1
Neeraj Gyawali <gyawalineeraj777@gmail.com>
2024-11-26 15:28:40 +0545

    added gateway to the microservice landscape

---
+++ ng-ecommerce/ng-gateway/src/main/java/com/ng/gateway/NgGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ng.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgGatewayApplication.class, args);
+	}
+
+}
